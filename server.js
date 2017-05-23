75b1a76dc0ca91ed9a429a5ff232dc638d807395
Ajay Mohan <ajay.mohan@intellectdesign.com>
2017-05-23 16:00:38 +0530

    update API added

--- server.js
+++ server.js
@@ -41,6 +41,33 @@ app.delete('/todos/:id',function(req,res){
 	}
 });
 
+app.put('/todos/:id',function(req,res){
+	var requiredId = parseInt(req.params.id);
+	var matchedRecord = _.findWhere(todos,{id:requiredId});
+	var body = _.pick(req.body,'description','completed');// use ._pick to only pick the required keys from request
+	var validAttributes = {};
+
+	if(!matchedRecord){
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	}else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	}else if(body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+	_.extend(matchedRecord,validAttributes);
+	res.json(matchedRecord);
+
+
+});
+
 app.post('/todos',function(req,res){
 	var body = _.pick(req.body,'description','completed');// use ._pick to only pick the required keys from request
 	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
