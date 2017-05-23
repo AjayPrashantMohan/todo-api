fc90a82791a0bb9db13eaa783be5193856783636
Ajay Mohan <ajay.mohan@intellectdesign.com>
2017-05-23 13:12:38 +0530

    Refactoring API with underscore js

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
-/*var todos = [{
-	"id":1,
-	"description":"lunch",
-	"completed":false
-},
-{
-	"id":2,
-	"description":"dinner",
-	"completed":false
-}];*/
 var todos = [];
 var todoNextId = 1;
 
@@ -27,12 +18,7 @@ app.get('/todos',function(req,res){
 
 app.get('/todos/:id',function(req,res){
 	var requiredId = parseInt(req.params.id);
-	var matchedRecord;
-	todos.forEach(function(todo){
-		if(todo.id === requiredId){
-			matchedRecord = todo;
-		}
-	});
+	var matchedRecord = _.findWhere(todos,{id:requiredId});
 	if(matchedRecord){
 		res.json(matchedRecord);
 	}
@@ -42,7 +28,11 @@ app.get('/todos/:id',function(req,res){
 });
 
 app.post('/todos',function(req,res){
-	var body = req.body;
+	var body = _.pick(req.body,'description','completed');// use ._pick to only pick the required keys from request
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();// 400 is a status for not sending all the required inputs in the request
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId;
 	todos.push(body);
 	res.send("User with id "+body.id+" is successfully added");
